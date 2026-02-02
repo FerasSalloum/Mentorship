be8425ac438aadac384deee78acf982f3fde6214
FerasSalloum <ferassalloum1999l@gmail.com>
2026-02-03 02:52:45 +0300

    use onclick instead of addEventListener in application9

--- application9/index.js
+++ application9/index.js
@@ -32,36 +32,35 @@ const closeEditModal = () => {
     idetContaner.setAttribute("style", "visibility: hidden;")
 }
 const showbroduct = () => {
-    cardContaner.innerHTML = broductshow.map((item) => {
+    cardContaner.innerHTML = broductshow.map((item, index) => {
         return item = `<div class="card" data-product="${item.name}">
                 <h2>${item.title}</h2>
                 <p>${item.info}</p>
                 <p>السعر: ${item.price}</p>
                 <div class="button-contaner">
-                <button class="idet">تعديل</button>
-                <button class="dilit">حذف</button>
+                <button onclick="iditeProduct(${index})" class="idet">تعديل</button>
+                <button onclick="deleteProdect(${index})" class="dilit">حذف</button>
                 </div>
                 </div>`
     }).join(" ")
 
 }
-const iditeProduct = (e) => {
+const iditeProduct = (index) => {
     idetContaner.setAttribute("style", "visibility: visible;")
-    const productName = e.target.closest(".card").dataset.product
-    const item = broductshow.find((item) => item.name == productName)
+    const broduct = broductshow[index]
     idetContaner.innerHTML = ` 
             <form class="idet-card" id="formIdit" >
                 <i id="close-buton"  class="fa-solid fa-circle-xmark"></i>
-                <input id="title" type="text" required value="${item.title}" placeholder="اسم المنتج">
-                <input id="info" type="text" required value="${item.info}" placeholder="معلومات المنتج">
-                <input id="praes" type="number" required min="0" value="${item.price}" placeholder="سعر المنتج">
+                <input id="title" type="text" required value="${broduct.title}" placeholder="اسم المنتج">
+                <input id="info" type="text" required value="${broduct.info}" placeholder="معلومات المنتج">
+                <input id="praes" type="number" required min="0" value="${broduct.price}" placeholder="سعر المنتج">
                 <button type="submit">حفظ</button>
             </form>
         `
     document.getElementById("formIdit").addEventListener("submit", (e) => {
         e.preventDefault()
         broductshow.map((item) => {
-            if (item.name == productName) {
+            if (item.name == broduct.name) {
                 item.title = document.getElementById("title").value
                 item.info = document.getElementById("info").value
                 item.price = parseInt(document.getElementById("praes").value)
@@ -74,13 +73,8 @@ const iditeProduct = (e) => {
         closeEditModal()
     })
 }
-cardContaner.addEventListener("click", (e) => {
-    if (e.target.classList.contains("dilit")) {
-        const productName = e.target.closest(".card").dataset.product
-        broductshow = broductshow.filter((item) => item.name != productName)
-        return showbroduct()
-    } else if (e.target.classList.contains("idet")) {
-        iditeProduct(e)
-    }
-})
+const deleteProdect = (index) => {
+    broductshow.splice(index, 1)
+    showbroduct()
+}
 showbroduct()
